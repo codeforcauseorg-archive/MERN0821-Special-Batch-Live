07322659beacc1f1548d1c618410fecfae58e68d
KeenWarrior <anujgargcse@gmail.com>
2021-09-01 16:21:01 +0530

    l5

---
+++ lecture-05/stringIntro.js
@@ -0,0 +1,13 @@
+let hello = "Welcome to Code for Cause";
+
+// for(let item of hello){
+//     console.log(item);
+// }
+
+// console.log(hello[6]);
+
+hello[1] = "t";
+console.log(hello);
+
+
+// console.log(hello);
